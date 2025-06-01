7cc5f585be9af30507ae52613db1f19d6a1f33d6
Ashwin Kapile <ashwinkapile2002@gmail.com>
2025-06-01 18:43:00 +0530

    added: judge_square_sum problem (LeetCode 633)

---
+++ DSA-practise/math/judge_square_sum.js
@@ -0,0 +1,19 @@
+// Problem: LeetCode #633 - Sum of Square Numbers
+// Difficulty: Medium
+// Topic: Math, Brute Force
+// Link: https://leetcode.com/problems/sum-of-square-numbers/
+
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function (c) {
+  for (let a = 0; a * a <= c; a++) {
+    let bSquared = c - a * a;
+    let b = Math.sqrt(bSquared);
+    if (Number.isInteger(b)) {
+      return true;
+    }
+  }
+  return false;
+};
