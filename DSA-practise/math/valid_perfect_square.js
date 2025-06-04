401c77550675a32ef95091d68c8d2b1a43d1bbe6
Ashwin Kapile <ashwinkapile2002@gmail.com>
2025-06-04 22:18:10 +0530

    initial commit: added math and recursion problems

--- DSA-practise/math/valid_perfect_square.js
+++
@@ -1,7 +0,0 @@
-// Problem: LeetCode #367 - Valid Perfect Square
-// Difficulty: Easy
-// Topic: Math,Binary Search, Brute Force
-// Link: https://leetcode.com/problems/valid-perfect-square/description/
-
-/**
- 
